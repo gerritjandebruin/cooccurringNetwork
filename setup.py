0186dcde2c62b9ecfa8eb278b68e157d6c775e63
gerritjandebruin <33900424+gerritjandebruin@users.noreply.github.com>
2020-12-11 12:28:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cooccurringNetwork",
-    version="0.0.1",
+    version="1.1",
     author="Gerrit-Jan de Bruin",
     author_email="gerritjan.debruin@gmail.com",
     description="Module to create cooccuring networks from event data.",
